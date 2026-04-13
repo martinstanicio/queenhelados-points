e838b2233d96b60b54a47b603b77683b16d41a31
Martín Stanicio <martuytimo@gmail.com>
2026-04-13 22:34:20 +0000

    create basic orchestrator

---
+++ src/lib/orchestrator.py
@@ -0,0 +1,81 @@
+import pandas as pd
+
+from file_processors.client_list import ClientListProcessor
+from file_processors.pos import POSProcessor
+from file_processors.sales_by_article import SalesByArticleProcessor
+from lib.excel_parser import ExcelParser
+from storage_adapters.storage_adapter import StorageAdapter
+from util.path import get_path_components
+
+
+class Orchestrator:
+    def __init__(self, storage: StorageAdapter, parser: ExcelParser) -> None:
+        self.storage = storage
+        self.parser = parser
+        self.sales_by_article_processor = SalesByArticleProcessor()
+        self.client_list_processor = ClientListProcessor()
+        self.pos_processor = POSProcessor()
+
+    def get_denormalized_data(self) -> pd.DataFrame:
+        df_pos = self.pos_processor.process(
+            self.parser.parse(self.storage.open_file("puntos-de-venta.xlsx"))
+        )
+
+        all_files = self.storage.list_files()
+        list_sales_dfs: list[pd.DataFrame] = []
+        list_client_dfs: list[pd.DataFrame] = []
+
+        for file_path in all_files:
+            path_components = get_path_components(file_path)
+
+            if len(path_components) < 2:
+                continue
+
+            subfolder = path_components[0]
+            try:
+                match subfolder:
+                    case "ventas-por-articulo":
+                        df_raw = self.parser.parse(self.storage.open_file(file_path))
+                        list_sales_dfs.append(
+                            self.sales_by_article_processor.process(df_raw)
+                        )
+
+                    case "listado-de-clientes":
+                        file_name = path_components[1]
+                        branch_id = file_name.split(".")[0]
+                        df_raw = self.parser.parse(self.storage.open_file(file_path))
+                        df_clients = self.client_list_processor.process(df_raw)
+                        df_clients["branch_id"] = branch_id
+                        list_client_dfs.append(df_clients)
+            except Exception as e:
+                print(f"[!] Error processing {file_path}: {e}")
+
+        if not list_sales_dfs or not list_client_dfs:
+            return pd.DataFrame()
+
+        df_sales = pd.concat(list_sales_dfs, ignore_index=True)
+        df_clients = pd.concat(list_client_dfs, ignore_index=True)
+
+        df_sales_pos = pd.merge(df_sales, df_pos, on="pos_id", how="left")
+        df_sales_pos_clients = pd.merge(
+            df_sales_pos, df_clients, on=["client_number", "branch_id"], how="left"
+        )
+
+        df = (
+            df_sales_pos_clients.groupby(
+                ["document_type", "tax_condition", "pos_id", "document_number"]
+            )
+            .agg(
+                {
+                    "date": "first",
+                    "time": "first",
+                    "branch_id": "first",
+                    "client_number": "first",
+                    "client_email": "first",
+                    "total_price": "sum",
+                }
+            )
+            .reset_index()
+        )
+
+        return df
--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
+import os
+
 from dotenv import load_dotenv
+
+from lib.excel_parser import ExcelParser
+from lib.orchestrator import Orchestrator
+from storage_adapters.gdrive import GoogleDriveAdapter
 
 
 def main() -> None:
     load_dotenv()
+
+    FOLDER_ID = os.environ.get("FOLDER_ID")
+    TARGET_SERVICE_ACCOUNT = os.environ.get("TARGET_SERVICE_ACCOUNT")
+
+    if not FOLDER_ID or not TARGET_SERVICE_ACCOUNT:
+        raise ValueError(
+            "Environment variables FOLDER_ID and TARGET_SERVICE_ACCOUNT must be set."
+        )
+
+    storage = GoogleDriveAdapter(FOLDER_ID, TARGET_SERVICE_ACCOUNT)
+    excel_parser = ExcelParser()
+    orchestrator = Orchestrator(storage, excel_parser)
+
+    df = orchestrator.get_denormalized_data()
+
 
 
 if __name__ == "__main__":
